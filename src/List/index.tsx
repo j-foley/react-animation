a2baa44717fe6bbffb79b87a2e414fa03f2f4606
John Foley <john.foley@ivx.com>
2022-06-22 13:59:29 +0200

    Animate List

--- src/List/index.tsx
+++ src/List/index.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import React, { useState } from 'react'
 import './style.css'
 
@@ -11,20 +12,33 @@ export const List = () => {
     })
   const handleRemove = (index: number) => setList((prev) => prev.filter((_, i) => index !== i))
 
+  const itemVariant = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: -100 }
+  }
+
   return (
     <>
       <button type='button' onClick={handleAdd}>
         Add item
       </button>
       <ul>
-        {list.map((item, index) => (
-          <li key={`item-${item}`}>
-            <div>List item {item + 1}</div>
+        <AnimatePresence>
+          {list.map((item, index) => (
+            <motion.li
+              key={`item-${item}`}
+              initial='hidden'
+              animate='visible'
+              exit='hidden'
+              variants={itemVariant}
+            >
+              <div>List item {item + 1}</div>
             <button type='button' onClick={() => handleRemove(index)}>
               X
             </button>
-          </li>
-        ))}
+            </motion.li>
+          ))}
+        </AnimatePresence>
       </ul>
     </>
   )
