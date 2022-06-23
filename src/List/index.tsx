373d4db434f87db0281968ec80de859178a55fc3
John Foley <john.foley@ivx.com>
2022-06-23 16:54:21 +0200

    Animate list

--- src/List/index.tsx
+++ src/List/index.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
+import { animated, useTransition } from '@react-spring/web'
 import './style.css'
 
 export const List = () => {
   const [list, setList] = useState([...Array(5).keys()])
+
+  const transition = useTransition(list, {
+    from: { opacity: 0, marginTop: 5 },
+    enter: { opacity: 1, maxHeight: 50, marginTop: 5 },
+    leave: { opacity: 0, maxHeight: 0, marginTop: 0 }
+  })
 
   const handleAdd = () =>
     setList((prev) => {
@@ -17,13 +24,13 @@ export const List = () => {
         Add item
       </button>
       <ul>
-        {list.map((item, index) => (
-          <li key={`item-${item}`}>
+        {transition((style, item, _, index) => (
+          <animated.li style={style} key={`item-${item}`}>
             <div>List item {item + 1}</div>
             <button type='button' onClick={() => handleRemove(index)}>
               X
             </button>
-          </li>
+          </animated.li>
         ))}
       </ul>
     </>
